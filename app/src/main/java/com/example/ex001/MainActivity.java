e89a47761f6b91799ad33256e70cddcbb11d478c
shaha <sy4172@bs.amalnet.k12.il>
2020-01-29 21:37:10 +0200

    everything is done! 1st app ever!

---
+++ app/src/main/java/com/example/ex001/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ex001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+    }
+
+    public void change(View view) {
+
+        btn.setText("oh, yea, I’ve been clicked!");
+    }
+}
